9245cf0303a66ecbe788dbc3495ff6a701bffef4
Himmel König <itogreniyorum@gmail.com>
2021-10-23 17:36:31 +0300

    bug fixed

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
 
         if not email:
-            raise ValueError('Email is mandatory')
+            raise ValueError('Email field is mandatory')
         # if not username:
         #     raise ValueError('Users must have a username')
         email = self.normalize_email(email)
@@ -15,7 +15,20 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        # CustomUser.objects.create_superuser(email = 'barry2@clarusway.com', password = '1234', is_superuser = False)
 
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+    
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField('email address', unique=True)
     # username = models.CharField(max_length=30, unique=True)
@@ -28,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    # objects = CustomUserManager()
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
